e50d30cef38738053fb1ea2ae98a387e7b6a8bcd
Matsushin <matsu1983@gmail.com>
2020-04-29 19:36:50 +0900

    annotateを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  title       :string           not null
+#  body        :text             not null
+#  category_id :integer          not null
+#  priority    :integer          not null
+#  status      :string           not null
+#  notice      :boolean          default(FALSE), not null
+#  code        :string
+#  limited_on  :date
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
