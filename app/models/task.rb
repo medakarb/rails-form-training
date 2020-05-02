a46edfb24b1e3e432fa34bd06eb7ccd66c87780f
Matsushin <matsu1983@gmail.com>
2020-05-02 20:09:40 +0900

    トレーニング用にコードを削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -18,17 +18,4 @@ class Task < ApplicationRecord
   extend Enumerize
   belongs_to :category
 
-  enumerize :status, in: %i(enabled disabled)
-
-  validates :title, presence: true, length: { maximum: 30 }
-  validates :body, presence: true
-  validates :priority, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :status, presence: true
-  validates :code, uniqueness: true, format: { with: /\A[0-9a-zA-Z]{6,}\z/ }, allow_blank: true
-
-  validate :require_future, if: proc { |record| record.limited_on != record.limited_on_was }
-
-  def require_future
-    errors.add(:limited_on, 'は未来の日付に設定してください') if limited_on.present? && limited_on < Date.current
-  end
 end
